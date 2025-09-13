554423abcb56adc5861559f379ffdf362e3387c1
Alcides Suarez <alcides_suarez1@hotmail.com>
2025-09-12 21:51:42 -0700

    2nd commit

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
 function convertToCelsius(f) {
-  return ((f - 32) * 5) / 9;
+  return (f - 32) * 5 / 9;
 }
 
 function describeTemperature(f) {
-  return alert;
-}
+  const c = convertToCelsius(f);
+  
+  let description = "";
+
+  if (c < 0) {
+    description = "very cold";
+  } else if (c < 20) {
+    description = "cold";
+  } else if (c < 30) {
+    description = "warm";
+  } else if (c < 40) {
+    description = "hot";
+  } else {
+    description = "very hot";
+  }
+
+  return `${f}F is ${c}C. That is ${description}.`;
+};
+
+const f = prompt("Please enter a temperature in Fahrenheit");
+const description = describeTemperature(f);
+
+alert(description);
